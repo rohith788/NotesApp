937e04fdda2b61ef82a6b401217bd1756cb7a46c
rohith788 <srohith788@gmail.com>
2020-12-06 16:46:46 -0500

    diplays data from firestore

--- src/components/card-grid/card-grid.component.jsx
+++ src/components/card-grid/card-grid.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import NoteCard from '../card/card.component'
-import NoteInputCard from '../note-input-card/note-input-card.component'
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+import NoteCard from "../card/card.component";
+import NoteInputCard from "../note-input-card/note-input-card.component";
+import { db } from "../../firebase/firebase.utils";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -12,47 +14,67 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     // padding: theme.spacing(1),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
 
 export default function CardGrid() {
+  const [title, setTitle] = useState([]);
+  const [note, setNote] = useState({});
+
+  useEffect(() => {
+    let noteObj = {};
+    let titles = [];
+    const unsubscribe = db.collection("notes").onSnapshot((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        titles.push(doc.data().title);
+        noteObj[doc.data().title] = doc.data().note;
+      });
+      setNote(noteObj);
+      setTitle(titles);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [db]);
+
   const classes = useStyles();
 
-  function FormRow() {
-    return (
-      <React.Fragment>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}><NoteCard /></Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}><NoteCard /></Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}><NoteCard /></Paper>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+  const FormRow = () => {
+    console.log(title.length);
+    if (title.length > 0) {
+      title.map((t) => {
+        return (
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
+              <NoteCard title={title} noteText={note[title]} />
+            </Paper>
+          </Grid>
+        );
+      });
+    } else {
+      return <div></div>;
+    }
+  };
 
   return (
     <div className={classes.root}>
-      <Grid container justify="center" alignItems="center" >
-        <NoteInputCard /> 
+      <Grid container justify="center" alignItems="center">
+        <NoteInputCard />
       </Grid>
       <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
-        <Grid container item xs={12} spacing={3}>
-          <FormRow />
-        </Grid>
+        {title.map((t) => {
+          console.log(t);
+          return (
+            <Grid item xs={4}>
+              <Paper className={classes.paper}>
+                <NoteCard title={t} noteText={note[t]} />
+              </Paper>
+            </Grid>
+          );
+        })}
       </Grid>
     </div>
   );
-
 }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,45 +1,42 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
     fontSize: 14,
   },
   pos: {
-    marginBottom: 12, 
+    marginBottom: 12,
   },
 });
 
-export default function NoteCard() {
-    const classes = useStyles();
-  
-    return (
-      <Card className={classes.root} variant="outlined">
-        <CardContent>
-          <Typography className={classes.title} gutterBottom>
-            Title
-          </Typography>
-          <Typography variant="h5">
-            Note Starts here
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    );
-  }
+export default function NoteCard({ title, noteText }) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography className={classes.title} gutterBottom>
+          {title}
+        </Typography>
+        <Typography variant="h5">{noteText}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
