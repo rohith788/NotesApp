10384ff03fd28fcafe377f4d3070b3f1d5350c55
rohith788 <srohith788@gmail.com>
2020-12-05 23:16:13 -0500

    note saves to firestore

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,33 +12,33 @@ import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MoreIcon from "@material-ui/icons/MoreVert";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   grow: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   title: {
     display: "none",
     [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
+      display: "block",
+    },
   },
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
     "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: "auto"
-    }
+      width: "auto",
+    },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
@@ -47,10 +47,10 @@ const useStyles = makeStyles(theme => ({
     pointerEvents: "none",
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   inputRoot: {
-    color: "inherit"
+    color: "inherit",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
@@ -59,21 +59,21 @@ const useStyles = makeStyles(theme => ({
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
-      width: "20ch"
-    }
+      width: "20ch",
+    },
   },
   sectionDesktop: {
     display: "none",
     [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
+      display: "flex",
+    },
   },
   sectionMobile: {
     display: "flex",
     [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  }
+      display: "none",
+    },
+  },
 }));
 
 export default function Header() {
@@ -84,7 +84,7 @@ export default function Header() {
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = event => {
+  const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
@@ -97,7 +97,7 @@ export default function Header() {
     handleMobileMenuClose();
   };
 
-  const handleMobileMenuOpen = event => {
+  const handleMobileMenuOpen = (event) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
@@ -155,7 +155,7 @@ export default function Header() {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            Material-UI
+            Keeps-Clone
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
@@ -165,7 +165,7 @@ export default function Header() {
               placeholder="Search…"
               classes={{
                 root: classes.inputRoot,
-                input: classes.inputInput
+                input: classes.inputInput,
               }}
               inputProps={{ "aria-label": "search" }}
             />
--- src/components/note-input-card/note-input-card.component.jsx
+++ src/components/note-input-card/note-input-card.component.jsx
@@ -1,84 +1,100 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import NoteTitleCard from '../note-title-bar/note-title-bar.component'
-import Collapse from '@material-ui/core/Collapse';
-import clsx from 'clsx';
-import NoteTextArea from '../note-text-area/note-text-area.component'
-import Button from '@material-ui/core/Button';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import clsx from "clsx";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
-import {db} from '../../firebase/firebase.utils'
+import { db } from "../../firebase/firebase.utils";
 
 const useStyles = makeStyles({
   root: {
-    '& .MuiTextField-root': {
-      width: '45ch',
+    "& .MuiTextField-root": {
+      width: "45ch",
+    },
+    "& .MuiInputBase-root": {
+      width: "45ch",
     },
     minWidth: 275,
     paddingBottom: 1,
-    boxShadow: "none"
+    boxShadow: "none",
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
     fontSize: 14,
   },
   pos: {
-    marginBottom: 2, 
+    marginBottom: 2,
   },
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
   },
 });
 
 export default function NoteInputCard() {
-    const classes = useStyles();
-    const [expanded, setExpanded] = React.useState(false); //expansion state of the card
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false); //expansion state of the card
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    }
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-    const saveNote = () => {
-      db.collection("notes").doc("note").set({
-        name: "Los Angeles",
-        state: "CA",
-        country: "USA"
-    })
-    .then(function() {
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-    }
-    return (
-      <Card className={classes.root} >
-        <CardActions disableSpacing>
-            <div className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-            })}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more">
-                <NoteTitleCard title='Title Here' />
-            </div>
-        </CardActions>
-        <div className='note_text'>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <center> 
-                  <NoteTextArea />
-                </center>
-                <right>
-                  <Button onClick={saveNote}>Default</Button>
-                </right>
-            </Collapse>
+  let titleInput = React.createRef();
+
+  const saveNote = () => {
+    db.collection("notes")
+      .add({
+        title: title,
+        note: note,
+      })
+      .then(handleExpandClick);
+  };
+  return (
+    <Card className={classes.root}>
+      <CardActions disableSpacing>
+        <div
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <TextField
+            ref={titleInput}
+            id="Title"
+            label="Title"
+            variant="outlined"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-      </Card>
-    );
-  }
+      </CardActions>
+      <div className="note_text">
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <center>
+            <TextField
+              id="outlined-multiline-static"
+              multiline
+              rows={4}
+              label="input note here"
+              variant="outlined"
+              onChange={(e) => setNote(e.target.value)}
+            />
+          </center>
+          <right>
+            <Button onClick={saveNote}>Add</Button>
+          </right>
+        </Collapse>
+      </div>
+    </Card>
+  );
+}
