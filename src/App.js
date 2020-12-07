dec74d61695100e1fff4f05daf3fd3cbdeb8761c
rohith788 <srohith788@gmail.com>
2020-12-07 02:26:45 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import './App.css';
-import CardGrid from './components/card-grid/card-grid.component'
-import Header from './components/header/header'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import CardGrid from "./components/card-grid/card-grid.component";
+import Header from "./components/header/header";
 
 function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+const HomePage = () => {
   return (
     <div>
       <Header />
       <CardGrid />
     </div>
   );
-}
+};
 
 export default App;
